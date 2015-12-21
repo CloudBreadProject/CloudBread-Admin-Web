c4dcf9756dfd68d63677a842cc047d19c35616fe
beingbook@gmail.com <beingbook@gmail.com>
2015-12-21 16:39:11 +0900

    Add express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('test');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  const { port } = server.address();
+  console.log(`The server is listening at http://localhost:${port}`);
+  if (__DEV__) {
+    console.log('__DEV_START__');
+  }
+});
