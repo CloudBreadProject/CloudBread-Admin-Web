e5578fe578cc66c6f5e8f351bdf2b791374b69d5
beingbook@gmail.com <beingbook@gmail.com>
2015-12-22 21:07:49 +0900

    Update clean task

--- tools/tasks/clean.js
+++ tools/tasks/clean.js
@@ -1,10 +1,7 @@
 import del from 'del';
-import { mkdirp } from '../lib/fs';
 
 async function clean() {
   await del(['.tmp', 'build/*'], { dot: true });
-  await mkdirp('build/assets');
-  await mkdirp('build/public');
 }
 
 export default clean;
