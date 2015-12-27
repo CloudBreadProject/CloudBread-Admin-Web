b6e7581600281a7fba80221aca4620c67c61e51f
beingbook@gmail.com <beingbook@gmail.com>
2015-12-28 03:27:08 +0900

    Add material-ui

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import routes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 const appContainer = document.getElementById('app');
 
