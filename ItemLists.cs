c55069ec7f3879967afa16c44ddc1bd3cfd74134
Yoon Seok <yoonseok417@gmail.com>
2016-03-09 01:57:04 +0900

    Modified itemlist model's attr

--- ItemLists.cs
+++ ItemLists.cs
@@ -32,5 +32,13 @@ namespace CloudBread_Admin_Web
         public string sCol8 { get; set; }
         public string sCol9 { get; set; }
         public string sCol10 { get; set; }
+        public string IteamCreateAdminID { get; set; }
+        public string IteamUpdateAdminID { get; set; }
+        public string HideYN { get; set; }
+        public string DeleteYN { get; set; }
+        public System.DateTimeOffset CreatedAt { get; set; }
+        public System.DateTimeOffset UpdatedAt { get; set; }
+        public string DataFromRegion { get; set; }
+        public System.DateTimeOffset DataFromRegionDT { get; set; }
     }
 }
