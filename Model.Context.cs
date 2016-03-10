496421879044d38d5101d788509a4763151fca83
Yoon Seok <yoonseok417@gmail.com>
2016-03-11 02:30:57 +0900

    Deleted coupon model attr

--- Model.Context.cs
+++ Model.Context.cs
@@ -25,10 +25,25 @@ namespace CloudBread_Admin_Web
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<ServerInfo> ServerInfo { get; set; }
+        public virtual DbSet<AdminMembers> AdminMembers { get; set; }
+        public virtual DbSet<CloudBreadAdminLog> CloudBreadAdminLog { get; set; }
+        public virtual DbSet<CloudBreadErrorLog> CloudBreadErrorLog { get; set; }
+        public virtual DbSet<CloudBreadLog> CloudBreadLog { get; set; }
+        public virtual DbSet<Coupon> Coupon { get; set; }
+        public virtual DbSet<CouponMember> CouponMember { get; set; }
+        public virtual DbSet<GameEventMember> GameEventMember { get; set; }
+        public virtual DbSet<GameEvents> GameEvents { get; set; }
         public virtual DbSet<GiftDepositories> GiftDepositories { get; set; }
         public virtual DbSet<ItemLists> ItemLists { get; set; }
-        public virtual DbSet<Sessions> Sessions { get; set; }
+        public virtual DbSet<MemberAccountBlockLog> MemberAccountBlockLog { get; set; }
+        public virtual DbSet<MemberGameInfoes> MemberGameInfoes { get; set; }
+        public virtual DbSet<MemberGameInfoStages> MemberGameInfoStages { get; set; }
+        public virtual DbSet<MemberItemPurchases> MemberItemPurchases { get; set; }
+        public virtual DbSet<MemberItems> MemberItems { get; set; }
+        public virtual DbSet<Members> Members { get; set; }
         public virtual DbSet<Notices> Notices { get; set; }
+        public virtual DbSet<ServerInfo> ServerInfo { get; set; }
+        public virtual DbSet<Sessions> Sessions { get; set; }
+        public virtual DbSet<StatsData> StatsData { get; set; }
     }
 }
