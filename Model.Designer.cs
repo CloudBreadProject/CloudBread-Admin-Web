1597f4231eaffae718ea3f625c30bf61b5961f4c
Yoon Seok <yoonseok417@gmail.com>
2016-03-19 05:07:57 +0900

    Arranged model diagram, cs file and so on

--- Model.Designer.cs
+++ Model.Designer.cs
@@ -1,10 +1,10 @@
-﻿// 모델 'C:\Users\junghyun\Documents\GitHub\CloudBread-Admin-Web\Model.edmx'에 대해 T4 코드 생성이 사용됩니다. 
-// 레거시 코드 생성을 사용하려면 '코드 생성 전략' 디자이너 속성의 값을
-// 'Legacy ObjectContext'로 변경하십시오. 이 속성은 모델이 디자이너에서 열릴 때
-// 속성 창에서 사용할 수 있습니다.
+﻿// T4 code generation is enabled for model 'C:\Users\dwkim\Documents\GitHub\CloudBread-Admin-Web\Model.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
 
-// 컨텍스트 및 엔터티 클래스가 생성되지 않은 경우 빈 모델을 만들었기 때문일 수도 있지만
-// 사용할 Entity Framework 버전을 선택하지 않았기 때문일 수도 있습니다. 모델에 맞는 컨텍스트 클래스 및
-// 엔터티 클래스를 생성하려면 디자이너에서 모델을 열고 디자이너 화면에서 마우스 오른쪽 단추를 클릭한
-// 다음 '데이터베이스에서 모델 업데이트...', '모델에서 데이터베이스 생성...' 또는 '코드 생성 항목 추가...'를
-// 선택하십시오.
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
