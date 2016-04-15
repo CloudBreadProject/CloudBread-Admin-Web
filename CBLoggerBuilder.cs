d045c9e050529c88c1487094becfe87bca83e3a3
YoonSeok Hong <yshong93@outlook.com>
2016-04-15 20:23:05 +0900

    add CBLoggerBuilder

---
+++ CBLoggerBuilder.cs
@@ -0,0 +1,24 @@
+﻿using CloudBreadAdminWebAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+
+namespace CloudBread_Admin_Web
+{
+    public class CBLoggerBuilder
+    {
+        public enum LoggerType { GET, GETbyIID, PUT, POST, PATCH, DELETE };
+        public enum LevelType { INFO };
+
+        public static void Build(ref Logging.CBLoggers logMsg, ClaimsPrincipal claims,
+            string controllerName, LevelType levelType, LoggerType loggerType,  string message)
+        {
+            logMsg.memberID = CBAuth.getMemberID(claims);
+            logMsg.Level = levelType.ToString();
+            logMsg.Logger = controllerName + "-" + loggerType.ToString();
+            logMsg.Message = message;
+        }
+    }
+}
