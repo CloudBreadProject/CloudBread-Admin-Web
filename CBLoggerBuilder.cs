a1cef528e162b3529cd8a5c53807f728ff5ddce9
YoonSeok Hong <yshong93@outlook.com>
2016-04-19 14:21:45 +0900

    make Loggger Builder

--- CBLoggerBuilder.cs
+++ CBLoggerBuilder.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Web;
+using System.Web.Http.OData;
 
 namespace CloudBread_Admin_Web
 {
@@ -12,13 +13,45 @@ namespace CloudBread_Admin_Web
         public enum LoggerType { GET, GETbyIID, PUT, POST, PATCH, DELETE };
         public enum LevelType { INFO };
 
-        public static void Build(ref Logging.CBLoggers logMsg, ClaimsPrincipal claims,
-            string controllerName, LevelType levelType, LoggerType loggerType,  string message)
+        public static Logging.CBLoggers Build(string sid, LevelType level, string logger, string message)
         {
-            logMsg.memberID = CBAuth.getMemberID(claims);
-            logMsg.Level = levelType.ToString();
-            logMsg.Logger = controllerName + "-" + loggerType.ToString();
-            logMsg.Message = message;
+            Logging.CBLoggers logMessage = new Logging.CBLoggers();
+
+            logMessage.memberID = sid;
+            logMessage.Level = level.ToString();
+            logMessage.Logger = logger;
+            logMessage.Message = message;
+
+            return logMessage;
+        }
+
+        private string controllerTag;
+
+        public CBLoggerBuilder(string controllerTag)
+        {
+            this.controllerTag = controllerTag;
+        }
+
+        public Logging.CBLoggers build(ODataController controller, LevelType level, LoggerType type, string message = null)
+        {
+            string sid = CBAuth.getMemberID(controller.User as ClaimsPrincipal);
+            string logger = controllerTag + "-" + type.ToString();
+
+            if (message == null)
+            {
+                switch (type)
+                {
+                    case LoggerType.GET:
+                    case LoggerType.GETbyIID:
+                        message = controller.Request.RequestUri.PathAndQuery.ToString();
+                        break;
+                    default:
+                        message = "No Message";
+                        break;
+                }
+            }
+
+            return CBLoggerBuilder.Build(sid, level, logger, message);
         }
     }
 }
