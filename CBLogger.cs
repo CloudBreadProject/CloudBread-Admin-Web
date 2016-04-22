2fc0c8828f91000116eb1a3e2053ca414860f275
YoonSeok Hong <yshong93@outlook.com>
2016-04-22 14:36:07 +0900

    move builder to logging class

--- CBLogger.cs
+++ CBLogger.cs
@@ -23,12 +23,62 @@ using Newtonsoft.Json;
 using CloudBreadAdminWeb.globals;
 using Microsoft.Practices.TransientFaultHandling;
 using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
+using System.Web.Http.OData;
+using CloudBreadAdminWebAuth;
+using System.Security.Claims;
 //using CloudBreadRedis;
 
 namespace CloudBread_Admin_Web
 {
     public class Logging
     {
+
+        public class CBLoggerBuilder
+        {
+            public enum LoggerType { GET, GETbyIID, PUT, POST, PATCH, DELETE };
+            public enum LevelType { INFO };
+
+            public static Logging.CBLoggers Build(string sid, LevelType level, string logger, string message)
+            {
+                Logging.CBLoggers logMessage = new Logging.CBLoggers();
+
+                logMessage.memberID = sid;
+                logMessage.Level = level.ToString();
+                logMessage.Logger = logger;
+                logMessage.Message = message;
+
+                return logMessage;
+            }
+
+            private string _controllerTag;
+
+            public CBLoggerBuilder(string controllerTag)
+            {
+                this._controllerTag = controllerTag;
+            }
+
+            public Logging.CBLoggers build(ODataController controller, LevelType level, LoggerType type, string message = null)
+            {
+                string sid = CBAuth.getMemberID(controller.User as ClaimsPrincipal);
+                string logger = _controllerTag + "-" + type.ToString();
+
+                if (message == null)
+                {
+                    switch (type)
+                    {
+                        case LoggerType.GET:
+                        case LoggerType.GETbyIID:
+                            message = controller.Request.RequestUri.PathAndQuery.ToString();
+                            break;
+                        default:
+                            message = "No Message";
+                            break;
+                    }
+                }
+
+                return CBLoggerBuilder.Build(sid, level, logger, message);
+            }
+        }
 
         /**
         * @class CBLoggers 
