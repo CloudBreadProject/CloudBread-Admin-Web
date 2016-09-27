f06fcecfc277ee12c06b5f67bce0f1f3b43015bb
Jung-Soo Ahn <staringstar@gmail.com>
2016-09-27 23:45:56 +0900

    extract named routes boot.

---
+++ server/bootstraps/router-boot.js
@@ -0,0 +1,230 @@
+var Router = require('named-routes');
+
+function RouterBoot(expressApp, viewsPath) {
+    var router = new Router();
+    router.extendExpress(expressApp);
+    router.registerAppHelpers(expressApp);
+
+    expressApp.set('router', router);
+
+    expressApp.get('/dashboard/v1', 'dashboard.v1', function (req, res) {
+        res.render('dashboard/v1', {
+            title: 'Blank Page'
+        });
+    });
+
+    expressApp.get('/dashboard/v2', 'dashboard.v2', function (req, res) {
+        res.render('dashboard/v2', {
+            title: 'Blank Page'
+        });
+    });
+
+    expressApp.get('/user/profile', 'user.profile', function (req, res) {
+        res.render('user/profile', {
+            title: 'Profile'
+        });
+    });
+
+    expressApp.get('/widgets/index', 'widgets.index', function (req, res) {
+        res.render("widgets/index", {
+            title: "Widgets"
+        })
+    });
+
+    expressApp.get('/charts.chartjs', 'charts.chartjs', function(req, res){
+        res.render("charts/chart-js", {
+            title: "ChartJS"
+        })
+    });
+
+    expressApp.get('/charts/morris', 'charts.morris', function(reqt, res) {
+        res.render("charts/morris", {
+            title: "Morris"
+        })
+    });
+
+    expressApp.get('/charts/flot', 'charts.flot', function(reqt, res) {
+        res.render("charts/flot", {
+            title: "Flot"
+        })
+    });
+
+    expressApp.get('/ui-elements/general', 'ui-elements.general' ,function(req, res) {
+        res.render("ui-elements/general", {
+            title: "General"
+        })
+    });
+
+    expressApp.get('/ui-elements/icons', 'ui-elements.icons', function(req, res) {
+        res.render("ui-elements/icons", {
+            title: "Icons"
+        })
+    });
+    expressApp.get('/ui-elements/buttons', 'ui-elements.buttons', function(req, res) {
+        res.render("ui-elements/buttons", {
+            title: "Buttons"
+        })
+    });
+
+    expressApp.get('/ui-elements/sliders', 'ui-elements.sliders', function(req, res) {
+        res.render("ui-elements/sliders", {
+            title: "Sliders"
+        })
+    });
+
+    expressApp.get('/ui-elements/timeline', 'ui-elements.timeline', function(req, res) {
+        res.render("ui-elements/timeline", {
+            title: "Timeline"
+        })
+    });
+
+    expressApp.get('/ui-elements/modals', 'ui-elements.modals', function(req, res) {
+        res.render("ui-elements/modals", {
+            title: "Modals"
+        })
+    });
+
+
+    expressApp.get('/forms/general', 'forms.general' ,function(req, res) {
+        res.render("forms/general", {
+            title: "General"
+        });
+    });
+
+    expressApp.get('/forms/advanced', 'forms.advanced' ,function(req, res) {
+        res.render("forms/advanced", {
+            title: "Advanced"
+        });
+    });
+
+    expressApp.get('/forms/editors', 'forms.editors' ,function(req, res) {
+        res.render("forms/editors", {
+            title: "Editors"
+        });
+    });
+
+    expressApp.get('/tables/simple', 'tables.simple' ,function(req, res) {
+        res.render("tables/simple", {
+            title: "Simple"
+        });
+    });
+
+    expressApp.get('/tables/dataTables', 'tables.dataTables' ,function(req, res) {
+        res.render("tables/data-tables", {
+            title: "Data Tables"
+        });
+    });
+
+    expressApp.get('/calendar/index', 'calendar.index', function(req, res) {
+        res.render("calendar/index", {
+            title: "Calendar"
+        });
+    });
+
+    expressApp.get('/mailbox/index', 'mailbox.index' ,function(req, res) {
+        res.render("mailbox/index", {
+            title: "Mailbox"
+        });
+    });
+
+    expressApp.get('/mailbox/compose', 'mailbox.compose' ,function(req, res) {
+        res.render("mailbox/compose", {
+            title: "Compose"
+        });
+    });
+
+    expressApp.get('/mailbox/readMail', 'mailbox.readMail' ,function(req, res) {
+        res.render("mailbox/read-mail", {
+            title: "Read Mail"
+        });
+    });
+
+    expressApp.get('/examples/invoice', 'examples.invoice' , function(req, res) {
+        res.render("examples/invoice", {
+            title: "Invoice"
+        });
+    });
+
+    expressApp.get('/examples/invoicePrint', 'examples.invoicePrint' ,function(req, res) {
+        res.render("examples/invoice-print", {
+            title: "Invoice Print",
+            layout: false
+        });
+    });
+
+    expressApp.get('/charts/inlineCharts', 'charts.inlineCharts', function(reqt, res) {
+        res.render("charts/inlineCharts", {
+            title: "inlineCharts"
+        })
+    });
+
+    expressApp.get('/user/lockscreen', 'user.lockscreen' ,function(req, res) {
+        res.render('user/lockscreen', {
+            title: 'Lock Screen',
+            layout: 'lockscreen'
+        });
+    });
+
+    expressApp.get('/user/login', 'user.login' ,function(req, res) {
+        res.render('user/login', {
+            title: 'Login',
+            layout: 'auth'
+        });
+    });
+
+    expressApp.get('/user/register', 'user.register' ,function(req, res) {
+        res.render('user/register', {
+            title: 'Register',
+            layout: 'auth'
+        });
+    });
+
+    expressApp.get('/user/profile', 'user.profile' ,function(req, res) {
+        res.render('user/profile', {
+            title: 'Profile'
+        });
+    });
+
+    expressApp.get('/user/new', 'user.new' ,function(req, res) {
+        res.render('user/new', {
+            title: 'New User'
+        });
+    });
+
+    expressApp.get('/user/list', 'user.list', function (req, res) {
+        res.render("crud-user/list", {
+            title: 'List Users',
+            crud_user: results
+        });
+    });
+
+    expressApp.get('/login', 'login', function(req, res) {
+        res.render('user/login', {
+            title: 'Login',
+            layout: 'auth'
+        });
+    });
+
+    expressApp.get('/logout', 'logout', function(req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+
+    expressApp.get('/documentation', 'documentation.index' ,function(req, res) {
+        res.render("documentation/index", {
+            title: "Documentation",
+            layout: false
+        });
+    });
+
+    expressApp.get('/', function (req, res) {
+        res.render('home');
+        //res.render('/dashboard/index', {
+        //    title: 'Blank Page'
+        //});
+    });
+}
+
+module.exports = RouterBoot;
+
+
--- server/server.js
+++ server/server.js
@@ -2,19 +2,14 @@ var express = require('express');
 var session      = require('express-session');
 var bodyParser = require('body-parser');
 var path         = require('path');
-var Router       = require('named-routes');
 var HandlebarBoot   = require('./bootstraps/handlebar-boot');
+var RouterBoot   = require('./bootstraps/router-boot');
 
 var app = express();
 
 var alePath = path.join(__dirname, '../resources/admin-lte/');
 var viewsPath = alePath + 'views/';
 var assetsPath = alePath + 'assets/';
-
-// Config named routes
-var router = new Router();
-router.extendExpress(app);
-router.registerAppHelpers(app);
 
 // Config session
 app.use(session({
@@ -28,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 HandlebarBoot(app, viewsPath);
+RouterBoot(app);
 
 // Public assets
 app.use('/public', express.static(path.join(assetsPath)));
@@ -77,227 +73,6 @@ app.use(function(err, request, response, next) {
         title: title,
         layout: layout
     });
-});
-
-
-// Set router
-app.set('router', router);
-
-app.get('/dashboard/v1', 'dashboard.v1', function (req, res) {
-    res.render('dashboard/v1', {
-        title: 'Blank Page'
-    });
-});
-
-app.get('/dashboard/v2', 'dashboard.v2', function (req, res) {
-    res.render('dashboard/v2', {
-        title: 'Blank Page'
-    });
-});
-
-app.get('/user/profile', 'user.profile', function (req, res) {
-    res.render('user/profile', {
-        title: 'Profile'
-    });
-});
-
-app.get('/widgets/index', 'widgets.index', function (req, res) {
-    res.render("widgets/index", {
-        title: "Widgets"
-    })
-});
-
-app.get('/charts.chartjs', 'charts.chartjs', function(req, res){
-    res.render("charts/chart-js", {
-        title: "ChartJS"
-    })
-});
-
-app.get('/charts/morris', 'charts.morris', function(reqt, res) {
-    res.render("charts/morris", {
-        title: "Morris"
-    })
-});
-
-app.get('/charts/flot', 'charts.flot', function(reqt, res) {
-    res.render("charts/flot", {
-        title: "Flot"
-    })
-});
-
-app.get('/ui-elements/general', 'ui-elements.general' ,function(req, res) {
-    res.render("ui-elements/general", {
-        title: "General"
-    })
-});
-
-app.get('/ui-elements/icons', 'ui-elements.icons', function(req, res) {
-    res.render("ui-elements/icons", {
-        title: "Icons"
-    })
-});
-app.get('/ui-elements/buttons', 'ui-elements.buttons', function(req, res) {
-    res.render("ui-elements/buttons", {
-        title: "Buttons"
-    })
-});
-
-app.get('/ui-elements/sliders', 'ui-elements.sliders', function(req, res) {
-    res.render("ui-elements/sliders", {
-        title: "Sliders"
-    })
-});
-
-app.get('/ui-elements/timeline', 'ui-elements.timeline', function(req, res) {
-    res.render("ui-elements/timeline", {
-        title: "Timeline"
-    })
-});
-
-app.get('/ui-elements/modals', 'ui-elements.modals', function(req, res) {
-    res.render("ui-elements/modals", {
-        title: "Modals"
-    })
-});
-
-
-app.get('/forms/general', 'forms.general' ,function(req, res) {
-    res.render("forms/general", {
-        title: "General"
-    });
-});
-
-app.get('/forms/advanced', 'forms.advanced' ,function(req, res) {
-    res.render("forms/advanced", {
-        title: "Advanced"
-    });
-});
-
-app.get('/forms/editors', 'forms.editors' ,function(req, res) {
-    res.render("forms/editors", {
-        title: "Editors"
-    });
-});
-
-app.get('/tables/simple', 'tables.simple' ,function(req, res) {
-    res.render("tables/simple", {
-        title: "Simple"
-    });
-});
-
-app.get('/tables/dataTables', 'tables.dataTables' ,function(req, res) {
-    res.render("tables/data-tables", {
-        title: "Data Tables"
-    });
-});
-
-app.get('/calendar/index', 'calendar.index', function(req, res) {
-    res.render("calendar/index", {
-        title: "Calendar"
-    });
-});
-
-app.get('/mailbox/index', 'mailbox.index' ,function(req, res) {
-    res.render("mailbox/index", {
-        title: "Mailbox"
-    });
-});
-
-app.get('/mailbox/compose', 'mailbox.compose' ,function(req, res) {
-    res.render("mailbox/compose", {
-        title: "Compose"
-    });
-});
-
-app.get('/mailbox/readMail', 'mailbox.readMail' ,function(req, res) {
-    res.render("mailbox/read-mail", {
-        title: "Read Mail"
-    });
-});
-
-app.get('/examples/invoice', 'examples.invoice' , function(req, res) {
-    res.render("examples/invoice", {
-        title: "Invoice"
-    });
-});
-
-app.get('/examples/invoicePrint', 'examples.invoicePrint' ,function(req, res) {
-    res.render("examples/invoice-print", {
-        title: "Invoice Print",
-        layout: false
-    });
-});
-
-app.get('/charts/inlineCharts', 'charts.inlineCharts', function(reqt, res) {
-    res.render("charts/inlineCharts", {
-        title: "inlineCharts"
-    })
-});
-
-app.get('/user/lockscreen', 'user.lockscreen' ,function(req, res) {
-    res.render('user/lockscreen', {
-        title: 'Lock Screen',
-        layout: 'lockscreen'
-    });
-});
-
-app.get('/user/login', 'user.login' ,function(req, res) {
-    res.render('user/login', {
-        title: 'Login',
-        layout: 'auth'
-    });
-});
-
-app.get('/user/register', 'user.register' ,function(req, res) {
-    res.render('user/register', {
-        title: 'Register',
-        layout: 'auth'
-    });
-});
-
-app.get('/user/profile', 'user.profile' ,function(req, res) {
-    res.render('user/profile', {
-        title: 'Profile'
-    });
-});
-
-app.get('/user/new', 'user.new' ,function(req, res) {
-    res.render('user/new', {
-        title: 'New User'
-    });
-});
-
-app.get('/user/list', 'user.list', function (req, res) {
-    res.render("crud-user/list", {
-        title: 'List Users',
-        crud_user: results
-    });
-});
-
-app.get('/login', 'login', function(req, res) {
-    res.render('user/login', {
-        title: 'Login',
-        layout: 'auth'
-    });
-});
-
-app.get('/logout', 'logout', function(req, res) {
-    req.logout();
-    res.redirect('/login');
-});
-
-app.get('/documentation', 'documentation.index' ,function(req, res) {
-    res.render("documentation/index", {
-        title: "Documentation",
-        layout: false
-    });
-});
-
-app.get('/', function (req, res) {
-    res.render('home');
-    //res.render('/dashboard/index', {
-    //    title: 'Blank Page'
-    //});
 });
 
 module.exports = app;
