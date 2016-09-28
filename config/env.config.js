87946c03e3e290dace59dcf0c61bde2a1be71b6a
james.star <james.star@kakaocorp.com>
2016-09-28 21:08:42 +0900

    restructuring.

--- config/env.config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    development: require('./environments/development.json'),
-    production: require('./environments/production.json'),
-    test: require('./environments/test.json')
-};
-
-module.exports = config[process.env.NODE_ENV || 'development'];
-
---
+++ server/express.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var glob = require('glob');
+var session = require('express-session');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var methodOverride = require('method-override');
+
+var HandlebarBoot = require('./bootstraps/handlebar-boot');
+var RouterBoot = require('./bootstraps/router-boot');
+var AuthBoot = require('./bootstraps/auth-boot');
+
+
+
+module.exports = function(app, config) {
+    var env = process.env.NODE_ENV || 'development';
+    app.locals.ENV = env;
+    app.locals.ENV_DEVELOPMENT = env == 'development';
+
+    HandlebarBoot(app, config.root + '/server/views');
+    AuthBoot(app);
+    RouterBoot(app, AuthBoot);
+
+    // Public assets
+    app.use('/public', express.static(config.root + '/public'));
+    app.use('/admin', express.static(config.root + '/public'));
+
+    // app.use(favicon(config.root + '/public/img/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(methodOverride());
+
+    var controllers = glob.sync(config.root + '/server/controllers/*.js');
+    controllers.forEach(function (controller) {
+        require(controller)(app);
+    });
+
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    if(app.get('env') === 'development'){
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err,
+                title: 'error'
+            });
+        });
+    }
+
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {},
+            title: 'error'
+        });
+    });
+
+};
+
