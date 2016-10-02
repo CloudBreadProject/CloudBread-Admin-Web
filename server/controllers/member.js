24e154a4d83821e0ea15fb7e7da68ea308e2ab49
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-02 17:57:29 +0900

    edit member create page view.

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -4,9 +4,20 @@ function route(expressApp){
     expressApp.get('/member', 'member', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
         res.render('member/list', {
-            title: 'Generator-Express MVC',
-            articles: articles
+            title: 'Members',
+            listObjs: articles
         });
+    });
+
+    expressApp.get('/member/create', 'member.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('member/create', {
+            title: 'Members Create'
+        });
+    });
+
+    expressApp.post('/member/', 'member.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/member');
     });
 }
 
