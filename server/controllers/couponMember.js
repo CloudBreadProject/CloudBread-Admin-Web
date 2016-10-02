f480b16004770986b2bf28951e44acec00e0ab65
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-02 17:16:57 +0900

    apply auth restrict.

--- server/controllers/couponMember.js
+++ server/controllers/couponMember.js
@@ -1,7 +1,7 @@
 var    Article = require('../models/article');
 
 function route(expressApp){
-    expressApp.get('/couponMember', 'couponMember', function (req, res, next) {
+    expressApp.get('/couponMember', 'couponMember', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
         res.render('home', {
             title: 'Generator-Express MVC',
