9a10accc68667d0456bca88094a33338959e6770
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 16:06:56 +0900

    add admin member creation view.

--- server/controllers/adminMember.js
+++ server/controllers/adminMember.js
@@ -3,10 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/adminMember', 'adminMember', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('adminMember/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
+    });
+
+    expressApp.get('/adminMember/create', 'adminMember.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('adminMember/create', {
+            title: 'AdminMember Create'
+        });
+    });
+
+    expressApp.post('/adminMember/', 'adminMember.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/adminMember');
     });
 }
 
