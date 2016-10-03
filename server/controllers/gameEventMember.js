b0b832957b3cf95bace422830342142d8f73aaf2
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 17:35:42 +0900

    add gameEvent Member View.

--- server/controllers/gameEventMember.js
+++ server/controllers/gameEventMember.js
@@ -3,10 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/gameEventMember', 'gameEventMember', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('gameEventMember/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
+    });
+
+    expressApp.get('/gameEventMember/create', 'gameEventMember.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('gameEventMember/create', {
+            title: 'GameEventMember Create'
+        });
+    });
+
+    expressApp.post('/gameEventMember/', 'gameEventMember.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/gameEventMember');
     });
 }
 
