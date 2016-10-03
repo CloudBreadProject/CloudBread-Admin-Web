1c08d3dfbe8475fdeae216376894b0a2174caeed
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 17:45:15 +0900

    add gameEvent route and view page.

--- server/controllers/gameEvent.js
+++ server/controllers/gameEvent.js
@@ -3,11 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/gameEvent', 'gameEvent', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('gameEvent/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
     });
-}
+
+    expressApp.get('/gameEvent/create', 'gameEvent.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('gameEvent/create', {
+            title: 'GameEvent Create'
+        });
+    });
+
+    expressApp.post('/gameEvent/', 'gameEvent.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/gameEvent');
+    });}
 
 module.exports = route;
