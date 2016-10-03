fd676878a84cf1fc9cd1b87e7743f470d6dd2a88
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 18:17:57 +0900

    add Item List route and view page.

--- server/controllers/itemList.js
+++ server/controllers/itemList.js
@@ -3,10 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/itemList', 'itemList', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
-            title: 'Generator-Express MVC',
+        res.render('itemList/list', {
+            title: 'Item List',
             articles: articles
         });
+    });
+
+    expressApp.get('/itemList/create', 'itemList.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('itemList/create', {
+            title: 'itemList Create'
+        });
+    });
+
+    expressApp.post('/itemList/', 'itemList.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/itemList');
     });
 }
 
