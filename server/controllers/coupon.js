3a5e922d2d4f62b575b0a7afc167f4543dc56fa6
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 16:48:45 +0900

    add coupon create and list view page.

--- server/controllers/coupon.js
+++ server/controllers/coupon.js
@@ -3,10 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/coupon', 'coupon', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('coupon/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
+    });
+
+    expressApp.get('/coupon/create', 'coupon.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('coupon/create', {
+            title: 'Coupon Create'
+        });
+    });
+
+    expressApp.post('/coupon/', 'coupon.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/coupon');
     });
 }
 
