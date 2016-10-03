87cfbde9f1131c0eb5984e8d3c0e5a05a997fc87
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-03 18:07:00 +0900

    add Gift Depository route and view page.

--- server/controllers/giftDepository.js
+++ server/controllers/giftDepository.js
@@ -3,10 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/giftDepository', 'giftDepository', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
-            title: 'Generator-Express MVC',
+        res.render('giftDepository/list', {
+            title: 'Gift Depository',
             articles: articles
         });
+    });
+
+    expressApp.get('/giftDepository/create', 'giftDepository.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('giftDepository/create', {
+            title: 'giftDepository Create'
+        });
+    });
+
+    expressApp.post('/giftDepository/', 'giftDepository.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/giftDepository');
     });
 }
 
