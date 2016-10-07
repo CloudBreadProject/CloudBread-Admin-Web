c6480de6f5ac6f94e75dccdc620d6dc0d030fcee
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-07 21:20:18 +0900

    add member Item menu view and controller route.

--- server/controllers/memberItem.js
+++ server/controllers/memberItem.js
@@ -3,11 +3,23 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/memberItem', 'memberItem', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('memberItem/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
     });
+
+    expressApp.get('/memberItem/create', 'memberItem.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('memberItem/create', {
+            title: 'Coupon Create'
+        });
+    });
+
+    expressApp.post('/memberItem/', 'memberItem.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/coupon');
+    });
+
 }
 
 module.exports = route;
