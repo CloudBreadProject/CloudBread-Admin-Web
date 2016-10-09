f55a08efe78b03663d32234145e409c72cd122ca
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-09 14:32:26 +0900

    add notice route and view page.

--- server/controllers/notice.js
+++ server/controllers/notice.js
@@ -3,11 +3,21 @@ var    Article = require('../models/article');
 function route(expressApp){
     expressApp.get('/notice', 'notice', expressApp.restrict, function (req, res, next) {
         var articles = [new Article(), new Article()];
-        res.render('home', {
+        res.render('notice/list', {
             title: 'Generator-Express MVC',
             articles: articles
         });
     });
-}
+
+    expressApp.get('/notice/create', 'notice.create', expressApp.restrict, function (req, res, next) {
+        var articles = [new Article(), new Article()];
+        res.render('notice/create', {
+            title: 'GameEvent Create'
+        });
+    });
+
+    expressApp.post('/notice/', 'notice.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/purchase');
+    });}
 
 module.exports = route;
