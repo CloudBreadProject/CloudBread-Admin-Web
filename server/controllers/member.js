c1e2c9fcfee0575cdc061818be329e1419d2e3bc
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-10 20:56:48 +0900

    modify member routing.

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -1,12 +1,32 @@
-var    Article = require('../models/article');
-
 function route(expressApp){
-    expressApp.get('/member', 'member', expressApp.restrict, function (req, res, next) {
-        var articles = [new Article(), new Article()];
-        res.render('member/list', {
-            title: 'Members',
-            listObjs: articles
+
+    expressApp.get('/member', 'member', expressApp.restrict, function (req, res) {
+        expressApp.models.Members.findAll().then(function(results) {
+            res.render('member/list', {
+                title: 'Members',
+                listObjs: results
+            });
+        }).catch(function(err) {
+            next(err);
         });
+    });
+
+    expressApp.get('/member/:id', 'member.show', expressApp.restrict, function(req, res) {
+        var memberId = req.params.id;
+        expressApp.models.Members.findOne({
+            where: {
+                MemberID: memberId
+            }
+        })
+        .then(function(result){
+            res.render('member/edit', {
+                title: 'Members',
+                obj : result
+            });
+        }).catch(function(err) {
+            next(err);
+        });
+
     });
 
     expressApp.get('/member/create', 'member.create', expressApp.restrict, function (req, res, next) {
@@ -17,6 +37,10 @@ function route(expressApp){
     });
 
     expressApp.post('/member/', 'member.store', expressApp.restrict, function(req, res, next) {
+        res.redirect('/member');
+    });
+
+    expressApp.post('/member/edit', 'member.update', expressApp.restrict, function(req, res) {
         res.redirect('/member');
     });
 }
