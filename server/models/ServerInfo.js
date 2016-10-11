2e8967d2a30e9bee3bed99205ca3dd3047e1b28f
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-11 19:58:59 +0900

    add ServerInfo Model.

---
+++ server/models/ServerInfo.js
@@ -0,0 +1,29 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var ServerInfo = sequelize.define('ServerInfo', {
+    InfoID : { type : DataTypes.STRING, primaryKey: true, autoIncrement: false}
+
+    , FEServerLists : { type : DataTypes.TEXT}
+    , SocketServerLists : { type : DataTypes.TEXT}
+
+    , Version : { type : DataTypes.STRING}
+
+    , ResourceLink : { type : DataTypes.TEXT}
+    , EULAText : { type : DataTypes.TEXT}
+
+    , sCol1 : { type : DataTypes.TEXT}
+    , sCol2 : { type : DataTypes.TEXT}
+    , sCol3 : { type : DataTypes.TEXT}
+    , sCol4 : { type : DataTypes.TEXT}
+    , sCol5 : { type : DataTypes.TEXT}
+
+    , CreatedAt : { type : DataTypes.DATE}
+    , UpdatedAt : { type : DataTypes.DATE}
+    , DataFromRegion : { type : DataTypes.STRING}
+    , DataFromRegionDT : { type : DataTypes.DATE}
+  }, {
+    timestamps: false,
+    tableName: 'ServerInfo'
+  });
+  return ServerInfo;
+};
