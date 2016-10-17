8cbff053eafa3e13e6401fa6577f4b0bdf29d761
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-17 20:26:46 +0900

    add itemList model  creation logic.

--- server/controllers/itemList.js
+++ server/controllers/itemList.js
@@ -12,6 +12,12 @@ function route(expressApp){
         });
     });
 
+    expressApp.get('/itemList/create', 'itemList.create', expressApp.restrict, function (req, res) {
+        res.render('itemList/create', {
+            title: 'ItemLists Create'
+        });
+    });
+
     expressApp.get('/itemList/:id', 'itemList.show', expressApp.restrict, function(req, res) {
         var itemListId = req.params.id;
         expressApp.models.ItemList.findOne({
@@ -30,15 +36,42 @@ function route(expressApp){
 
     });
 
-    expressApp.get('/itemList/create', 'itemList.create', expressApp.restrict, function (req, res) {
+    expressApp.post('/itemList/', 'itemList.store', expressApp.restrict, function(req, res, next) {
+        var itemList = req.body.itemList;
+        expressApp.models.ItemList.create({
+            ItemListID  : itemList.ItemListID
+            , ItemName: itemList.ItemName
+            , ItemDescription : itemList.ItemDescription
+            , ItemPrice : itemList.ItemPrice
+            , ItemSellPrice : itemList.ItemSellPrice
+            , ItemCategory1 : itemList.ItemCategory1
+            , ItemCategory2 : itemList.ItemCategory2
+            , ItemCategory3 : itemList.ItemCategory3
 
-        res.render('itemList/create', {
-            title: 'ItemLists Create'
+            , sCol1 : itemList.sCol1 || ''
+            , sCol2 : itemList.sCol2 || ''
+            , sCol3 : itemList.sCol3 || ''
+            , sCol4 : itemList.sCol4 || ''
+            , sCol5 : itemList.sCol5 || ''
+            , sCol6 : itemList.sCol6 || ''
+            , sCol7 : itemList.sCol7 || ''
+            , sCol8 : itemList.sCol8 || ''
+            , sCol9 : itemList.sCol9 || ''
+            , sCol10 : itemList.sCol10 || ''
+
+            , ItemCreateAdminID    : itemList.ItemCreateAdminID || ''
+            , ItemUpdateAdminID    : itemList.ItemUpdateAdminID || ''
+            , HideYN : itemList.HideYN
+            , DeleteYN : itemList.DeleteYN
+            , DataFromRegion : itemList.DataFromRegion || ''
+            , DataFromRegionDT : itemList.DataFromRegionDT || '1900-01-01 00:00:00:000 +00:00'
+
+        }).then(function() {
+            res.redirect('/itemList');
+        }).catch(function(err) {
+            next(err);
         });
-    });
 
-    expressApp.post('/itemList/', 'itemList.store', expressApp.restrict, function(req, res) {
-        res.redirect('/itemList');
     });
 
     expressApp.post('/itemList/edit', 'itemList.update', expressApp.restrict, function(req, res) {
