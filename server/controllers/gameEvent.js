685c90cc0464807ebdfebde3b9fe659b6caaffb3
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-17 20:26:10 +0900

    add gameEvent model  creation logic.

--- server/controllers/gameEvent.js
+++ server/controllers/gameEvent.js
@@ -11,6 +11,12 @@ function route(expressApp){
         });
     });
 
+    expressApp.get('/gameEvent/create', 'gameEvent.create', expressApp.restrict, function (req, res) {
+        res.render('gameEvent/create', {
+            title: 'GameEvents Create'
+        });
+    });
+
     expressApp.get('/gameEvent/:id', 'gameEvent.show', expressApp.restrict, function(req, res) {
         var gameEventId = req.params.id;
         expressApp.models.GameEvents.findOne({
@@ -29,15 +35,51 @@ function route(expressApp){
 
     });
 
-    expressApp.get('/gameEvent/create', 'gameEvent.create', expressApp.restrict, function (req, res) {
+    expressApp.post('/gameEvent/', 'gameEvent.store', expressApp.restrict, function(req, res, next) {
+        var gameEvent = req.body.gameEvent;
+        expressApp.models.GameEvents.create({
+            GameEventID  : gameEvent.GameEventID
+            , EventCategory1 : gameEvent.EventCategory1
+            , EventCategory2 : gameEvent.EventCategory2
+            , EventCategory3 : gameEvent.EventCategory3
+            , ItemListID : gameEvent.ItemListID
 
-        res.render('gameEvent/create', {
-            title: 'GameEvents Create'
+            , ItemCount : gameEvent.ItemCount
+            , ItemStatus : gameEvent.ItemStatus
+            , TargetGroup : gameEvent.TargetGroup
+            , TargetOS : gameEvent.TargetOS
+            , TargetDevice : gameEvent.TargetDevice
+            , EventImageLink : gameEvent.EventImageLink
+            , Title : gameEvent.Title
+            , Content : gameEvent.Content
+
+            , sCol1 : gameEvent.sCol1 || ''
+            , sCol2 : gameEvent.sCol2 || ''
+            , sCol3 : gameEvent.sCol3 || ''
+            , sCol4 : gameEvent.sCol4 || ''
+            , sCol5 : gameEvent.sCol5 || ''
+            , sCol6 : gameEvent.sCol6 || ''
+            , sCol7 : gameEvent.sCol7 || ''
+            , sCol8 : gameEvent.sCol8 || ''
+            , sCol9 : gameEvent.sCol9 || ''
+            , sCol10 : gameEvent.sCol10 || ''
+
+            , EventDurationFrom : gameEvent.EventDurationFrom
+            , EventDurationTo  : gameEvent.EventDurationTo
+            , OrderNumber   : gameEvent.OrderNumber
+            , CreateAdminID    : gameEvent.CreateAdminID || ''
+
+            , HideYN : gameEvent.HideYN
+            , DeleteYN : gameEvent.DeleteYN
+            , DataFromRegion : gameEvent.DataFromRegion || ''
+            , DataFromRegionDT : gameEvent.DataFromRegionDT || '1900-01-01 00:00:00:000 +00:00'
+
+        }).then(function() {
+            res.redirect('/gameEvent');
+        }).catch(function(err) {
+            next(err);
         });
-    });
 
-    expressApp.post('/gameEvent/', 'gameEvent.store', expressApp.restrict, function(req, res) {
-        res.redirect('/gameEvent');
     });
 
     expressApp.post('/gameEvent/edit', 'gameEvent.update', expressApp.restrict, function(req, res) {
--- server/models/GameEvents.js
+++ server/models/GameEvents.js
@@ -36,12 +36,9 @@ module.exports = function(sequelize, DataTypes) {
 
     , HideYN : { type : DataTypes.BOOLEAN}
     , DeleteYN : { type : DataTypes.BOOLEAN}
-    , CreatedAt : { type : DataTypes.DATE}
-    , UpdatedAt : { type : DataTypes.DATE}
     , DataFromRegion : { type : DataTypes.STRING}
     , DataFromRegionDT : { type : DataTypes.DATE}
   }, {
-    timestamps: false,
     tableName: 'GameEvents'
   });
   return GameEvents;
