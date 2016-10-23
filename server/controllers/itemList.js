181a87e1c90a38f2d01628cf4eb509502ae62ae2
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-23 23:07:54 +0900

    modify list paginaton for item list.

--- server/controllers/itemList.js
+++ server/controllers/itemList.js
@@ -1,13 +1,35 @@
 function route(expressApp){
 
-    expressApp.get('/itemList', 'itemList', expressApp.restrict, function (req, res) {
-        expressApp.models.ItemList.findAll().then(function(results) {
-            res.render('itemList/list', {
-                title: 'ItemList',
-                listObjs: results
-            });
-        }).catch(function(err) {
-            console.dir(err);
+    expressApp.get('/itemList', 'itemList', expressApp.restrict, function (req, res, next) {
+        var perPage = 15;
+        var currentPage = Number(req.query.page || 1);
+        var filter = {};
+
+        var Model = expressApp.models.ItemList;
+
+        Model.findAll({
+                where : filter
+            })
+            .then(function(filteredModel) {
+                var count = filteredModel.length;
+
+                Model.findAll({
+                        where : filter,
+                        limit : perPage,
+                        offset : (currentPage <= 1) ? 0 : perPage*(currentPage-1)
+                    })
+                    .then(function (results) {
+                        res.render('itemList/list', {
+                            title: 'ItemList',
+                            listObjs: results,
+                            page:currentPage,
+                            count:count
+                        });
+                    }).catch(function (err) {
+                    next(err);
+                });
+
+            }).catch(function(err){
             next(err);
         });
     });
