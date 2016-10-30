64330586a80ef7ada8668875a049848c37a22dad
Jung-Soo Ahn <staringstar@gmail.com>
2016-10-31 00:01:25 +0900

    add home google charts.

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -2,9 +2,39 @@ function route(expressApp){
     expressApp.get('/', 'home', expressApp.restrict, function (req, res) {
 
         res.render('home', {
-            title: 'Generator-Express MVC',
-            articles: articles
+            title: 'Cloud Bread Admin Home'
         });
+    });
+
+    /**
+     *
+     *
+     */
+
+    expressApp.get('/odata/StatsDatas', expressApp.restrict, function (req, res, next) {
+        // top=10&filter=DAU
+        console.log(req.query);
+        var limit = Number(req.query.top || 1);
+        var keyword = req.query.filter;
+        var orderBy = 'Fields';
+        var desc = 'desc';
+
+        var filter = {CategoryName: keyword };
+        var Model = expressApp.models.StatsData;
+
+        Model.findAll({
+                where : filter,
+                limit : limit
+            })
+            .then(function(results) {
+                res.json({
+                    "odata.metadata": "http://localhost:3000/odata/$metadata#StatsDatas",
+                    "value": results
+                });
+            }).catch(function(err){
+            next(err);
+        });
+
     });
 }
 
